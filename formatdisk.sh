ca7e89f4aebc0f5f340956e57ac22dff7b869578
Kevin Schmittle <kevin@schmittle.net>
2016-02-15 11:55:28 -0700

    use .lodev file

--- formatdisk.sh
+++ formatdisk.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
+if [ ! -f .lodev ]; then
+	echo 'No loop opened'
+	exit 1
+fi
+read < .lodev
+
 echo 'Formatting disk to fat12'
-sudo mkfs.fat -I -F 12 -s 1 -S 512 -R 1 -r 512 -f 2 /dev/loop0 512
+sudo mkfs.fat -I -F 12 -s 1 -S 512 -R 1 -r 512 -f 2 $REPLY 512
