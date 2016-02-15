eefda53f748618feb666ca0324029d67df8b8ffa
Kevin Schmittle <kevin@schmittle.net>
2016-02-15 11:52:46 -0700

    use .lodev file

--- copystage2.sh
+++ copystage2.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
+if [ ! -f .lodev ]; then
+	echo 'No loop opened'
+	exit 1
+fi
+read < .lodev
+
 echo 'Copying stage2 boot'
-sudo mount /dev/loop0 /media/lo
-sudo cp bin/stage2 /media/lo/stage2.sys
-sudo umount /media/lo
+mkdir -p /media/oslo
+sudo mount $REPLY /media/oslo
+sudo cp bin/stage2 /media/oslo/stage2.sys
+sudo umount /media/oslo
